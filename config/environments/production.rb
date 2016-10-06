c18eb1245c5da4ceb15b89b30639085b3d24688a
Sam Thornton <sdthornton@live.com>
2016-10-06 14:51:44 -0700

    Update mailer to use correct host on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'https://www.questionbehavior.org' }
 
   config.action_mailer.smtp_settings = {
     address: ENV.fetch('SPARKPOST_SMTP_HOST'),
